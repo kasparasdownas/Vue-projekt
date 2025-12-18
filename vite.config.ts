16fcabf87177f2936dbf347e50f9f0923894a734
kasparasdownas <s235116@dtu.dk>
2025-12-18 11:12:16 +0100

    config ændring

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
+  base: '/Vue-projekt/',
   plugins: [vue(), vuetify()],
   server: {
     port: 5174,
